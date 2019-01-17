7658b36818f279cd0f261b2408c750ee1f40902a
CloakedGhost07 <Michaelkmartin88@gmail.com>
2019-01-17 11:45:16 -0600

    Create hello.c
    Added my major to follow my name.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Michael Martin
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael Martin\n");
+  printf("Computer Engineering\n")
+
+  return 0;
+}
